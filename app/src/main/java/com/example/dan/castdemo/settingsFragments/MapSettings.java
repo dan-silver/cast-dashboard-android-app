063ef1dd478d3429b7dfe42701b03d9649691872
Dan Silver <dannysilver3@gmail.com>
2016-02-11 23:07:17 -0800

    Google places picker!

--- app/src/main/java/com/example/dan/castdemo/settingsFragments/MapSettings.java
+++ app/src/main/java/com/example/dan/castdemo/settingsFragments/MapSettings.java
@@ -1,31 +1,140 @@
 package com.example.dan.castdemo.settingsFragments;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.dan.castdemo.MainActivity;
 import com.example.dan.castdemo.R;
 import com.example.dan.castdemo.Widget;
-import com.example.dan.castdemo.Widget_Table;
-import com.raizlabs.android.dbflow.sql.language.Select;
+import com.example.dan.castdemo.WidgetOption;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+import com.google.android.gms.maps.model.LatLng;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class MapSettings extends WidgetSettingsFragment {
+public class MapSettings extends WidgetSettingsFragment implements GoogleApiClient.OnConnectionFailedListener {
+
+    public static String LOCATION_LAT = "LOCATION_LAT";
+    public static String LOCATION_LONG = "LOCATION_LONG";
+    public static String LOCATION_NAME = "LOCATION_NAME";
+    public static String LOCATION_ADDRESS = "LOCATION_ADDRESS";
+
+    int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
+
+    WidgetOption locationLat;
+    WidgetOption locationLong;
+    WidgetOption locationNameOption;
+    WidgetOption locationAddrOption;
+
+    @Bind(R.id.location_name)
+    TextView locationName;
+
+    @Bind(R.id.location_addr)
+    TextView locationAddr;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.map_settings, container, false);
         ButterKnife.bind(this, view);
 
+        locationLat = widget.getOption(MapSettings.LOCATION_LAT);
+        locationLong = widget.getOption(MapSettings.LOCATION_LONG);
+        locationNameOption = widget.getOption(MapSettings.LOCATION_NAME);
+        locationAddrOption = widget.getOption(MapSettings.LOCATION_ADDRESS);
+
+        updateLocationText();
 
         return view;
     }
 
-    public static void init(Widget widget) {
-
+    public void updateLocationText() {
+        locationName.setText(locationNameOption.value);
+        locationAddr.setText(locationAddrOption.value);
     }
 
+    public static void init(Widget widget) {
+        WidgetOption locationLat = new WidgetOption();
+        WidgetOption locationLng = new WidgetOption();
+        WidgetOption locationLine1 = new WidgetOption();
+        WidgetOption locationLine2 = new WidgetOption();
+
+        locationLat.key = LOCATION_LAT;
+        locationLat.value = "47.6166143";
+        locationLat.associateWidget(widget);
+        locationLat.save();
+
+        locationLng.key = LOCATION_LONG;
+        locationLng.value = "-122.6558899";
+        locationLng.associateWidget(widget);
+        locationLng.save();
+
+
+        locationLine1.key = LOCATION_NAME;
+        locationLine1.value = "Somewhere I forgot l1";
+        locationLine1.associateWidget(widget);
+        locationLine1.save();
+
+
+        locationLine2.key = LOCATION_ADDRESS;
+        locationLine2.value = "line 2";
+        locationLine2.associateWidget(widget);
+        locationLine2.save();
+    }
+
+    @OnClick(R.id.get_map_location)
+    public void getLocation() {
+        try {
+            Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN).build(getActivity());
+            startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+            // TODO: Handle the error.
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(getContext(), data);
+                Log.i(MainActivity.TAG, "Place: " + place.getName());
+                LatLng location = place.getLatLng();
+
+                locationLat.value = Double.toString(location.latitude);
+                locationLong.value = Double.toString(location.longitude);
+                locationNameOption.value = place.getName().toString();
+                locationAddrOption.value = place.getAddress().toString();
+
+                updateLocationText();
+
+                locationLat.save();
+                locationLong.save();
+                locationNameOption.save();
+                locationAddrOption.save();
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
--- app/src/main/java/com/example/dan/castdemo/widgets/MapWidget.java
+++ app/src/main/java/com/example/dan/castdemo/widgets/MapWidget.java
@@ -3,10 +3,11 @@ package com.example.dan.castdemo.widgets;
 import android.content.Context;
 
 import com.example.dan.castdemo.Widget;
+import com.example.dan.castdemo.WidgetOption;
+import com.example.dan.castdemo.settingsFragments.MapSettings;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
 
 public class MapWidget extends UIWidget {
     public static String HUMAN_NAME = "Map";
@@ -18,13 +19,23 @@ public class MapWidget extends UIWidget {
     @Override
     public JSONObject getContent() throws JSONException {
         JSONObject json = new JSONObject();
-        json.put("long", "value1");
-        json.put("lat", "value2");
+        json.put("lat", widget.getOption(MapSettings.LOCATION_LAT));
+        json.put("lng", widget.getOption(MapSettings.LOCATION_LONG));
         return json;
     }
 
     @Override
     public String getWidgetPreviewSecondaryHeader() {
-        return "Traffic Map of Chesterfield, MO";
+        WidgetOption locationName = widget.getOption(MapSettings.LOCATION_NAME);
+        if (locationName != null) {
+            return widget.getOption(MapSettings.LOCATION_NAME).value;
+        }
+
+        WidgetOption locationAddress = widget.getOption(MapSettings.LOCATION_NAME);
+        if (locationAddress != null) {
+            return widget.getOption(MapSettings.LOCATION_ADDRESS).value;
+        }
+
+        return "Unknown Location";
     }
 }
